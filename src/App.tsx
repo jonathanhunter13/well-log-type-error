e87cc8e8ba10c76efdf7a5370c4e39d134eccdbf
Jonathan Hunter <172397772+jonathanhunter13@users.noreply.github.com>
2026-01-30 15:27:57 +0100

    Initial set up

---
+++ src/App.tsx
@@ -0,0 +1,45 @@
+import { useRef } from "react";
+import { LogViewer, ScaleTrack, StackedTrack } from "@equinor/videx-wellog";
+import "@equinor/videx-wellog/dist/styles/styles.css";
+
+const ex5 = async () => {
+  const cementLength = 10;
+  const names = ["CEM", "FORM", "Other"];
+  const l = ["A", "B", "C", "D"];
+  const arr = [];
+  let currentFrom = Math.random() * 100;
+  for (let index = 1; index <= cementLength; index++) {
+    const newTo = currentFrom + 100 + Math.random() * 100;
+    const name = `${names[Math.floor(Math.random() * names.length)]} ${Math.floor(Math.random() * 8)}${l[Math.floor(Math.random() * l.length)]}`;
+    const r = Math.random() * 255;
+    const g = Math.random() * 255;
+    const b = Math.random() * 255;
+    const area = { name, to: newTo, from: currentFrom, color: { r, g, b } };
+    arr.push(area);
+    currentFrom = newTo;
+  }
+  return arr;
+};
+
+export const SampleVidexWellogPlot = () => {
+  const isInitialized = useRef(false);
+
+  function initializePlot(div: HTMLDivElement) {
+    if (!div || isInitialized.current) return;
+    const viewer = new LogViewer(); // Error occurs here
+    const scaleTrack = new ScaleTrack("scale", { maxWidth: 60 });
+    const cementTrack = new StackedTrack("id", { data: ex5 });
+    viewer.init(div).setTracks([scaleTrack, cementTrack]);
+    isInitialized.current = true;
+  }
+
+  return (
+    <div ref={initializePlot} style={{ width: "500px", height: "500px" }} />
+  );
+};
+
+function App() {
+  return <SampleVidexWellogPlot />;
+}
+
+export default App;
